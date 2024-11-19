c7dc04d3e947da0d88bef4a90925fad50713aac8
Pollus Emanuel Zhudarak <emanuel@zdk.studio>
2024-11-19 10:24:54 -0300

    Estilizado o footer

--- src/components/footer/Footer.jsx
+++ src/components/footer/Footer.jsx
@@ -9,11 +9,11 @@ export default function Footer () {
             <section className={s.topFooter}>
                 <p>4002-8922</p>
                 <div>
-                    <FontAwesomeIcon icon={faFacebook} size='3x'/>
-                    <FontAwesomeIcon icon={faTwitter} size='3x'/>
-                    <FontAwesomeIcon icon={faYoutube} size='3x'/>
-                    <FontAwesomeIcon icon={faLinkedin} size='3x'/>
-                    <FontAwesomeIcon icon={faInstagram} size='3x'/>
+                    <FontAwesomeIcon icon={faFacebook} size='2x'/>
+                    <FontAwesomeIcon icon={faTwitter} size='2x'/>
+                    <FontAwesomeIcon icon={faYoutube} size='2x'/>
+                    <FontAwesomeIcon icon={faLinkedin} size='2x'/>
+                    <FontAwesomeIcon icon={faInstagram} size='2x'/>
                 </div>
             </section>
             <section className={s.bottomFooter}>
