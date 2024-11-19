7f9019776ca6dbb54d7057f047f2215cc7d14005
Pollus Emanuel Zhudarak <emanuel@zdk.studio>
2024-11-19 09:26:31 -0300

    Adicionado os ícones corretos das marcas no footer

--- src/components/footer/Footer.jsx
+++ src/components/footer/Footer.jsx
@@ -1,7 +1,7 @@
 import s from '../footer/footer.module.scss'
 import React from 'react-dom'
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
-import { faFacebook, faInstagram, faTwitter } from '@fortawesome/free-brands-svg-icons'
+import { faFacebook, faInstagram, faLinkedin, faTwitter, faYoutube } from '@fortawesome/free-brands-svg-icons'
 
 export default function Footer () {
     return (
@@ -11,6 +11,8 @@ export default function Footer () {
                 <div>
                     <FontAwesomeIcon icon={faFacebook} size='3x'/>
                     <FontAwesomeIcon icon={faTwitter} size='3x'/>
+                    <FontAwesomeIcon icon={faYoutube} size='3x'/>
+                    <FontAwesomeIcon icon={faLinkedin} size='3x'/>
                     <FontAwesomeIcon icon={faInstagram} size='3x'/>
                 </div>
             </section>
