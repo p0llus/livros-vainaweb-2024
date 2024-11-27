25db1484211f4e412e4f36128c7c80983c58a845
Pollus Emanuel Zhudarak <emanuel@zdk.studio>
2024-11-27 14:11:20 -0300

    Adicionado setor superior e inferior do Início

--- src/pages/inicio/Inicio.jsx
+++ src/pages/inicio/Inicio.jsx
@@ -3,11 +3,42 @@ import s from '../inicio/inicio.module.scss'
 export default function Inicio () {
     return (
         <main>
-            <section>
+            <section id='topSection'>
                 <h2>
                     VENHA FAZER PARTE DA MAIOR REDE DE DOAÇÃO
                 </h2>
-                <img src="" alt="" srcset="" />
+                <img src={} alt=""/>
+            </section>
+            <section id='bottomSection'>
+                <h2>
+                    Por que devo doar?
+                </h2>
+                <div>
+                    <figure>
+                        <img src={} alt=""/>
+                        <figcaption>
+
+                        </figcaption>
+                    </figure>
+                    <figure>
+                        <img src={} alt=""/>
+                        <figcaption>
+
+                        </figcaption>
+                    </figure>
+                    <figure>
+                        <img src={} alt=""/>
+                        <figcaption>
+
+                        </figcaption>
+                    </figure>
+                    <figure>
+                        <img src={} alt=""/>
+                        <figcaption>
+
+                        </figcaption>
+                    </figure>
+                </div>
             </section>
         </main>
     )
