94a8a9d53dff2ee91bb2803bcf7734c0b121fa2e
Pollus Emanuel Zhudarak <emanuel@zdk.studio>
2024-12-10 16:17:02 -0300

    Estilizado a página Início

--- src/pages/inicio/Inicio.jsx
+++ src/pages/inicio/Inicio.jsx
@@ -1,4 +1,8 @@
 import s from '../inicio/inicio.module.scss'
+import img1 from '../../assets/img1.png'
+import img2 from '../../assets/img2.png'
+import img3 from '../../assets/img3.png'
+import img4 from '../../assets/img4.png'
 
 export default function Inicio () {
     return (
@@ -7,7 +11,6 @@ export default function Inicio () {
                 <h2>
                     VENHA FAZER PARTE DA MAIOR REDE DE DOAÇÃO
                 </h2>
-                {/* <img src={} alt=""/> */}
             </section>
             <section className={s.bottomSection}>
                 <h2>
@@ -15,27 +18,27 @@ export default function Inicio () {
                 </h2>
                 <div>
                     <figure>
-                        {/* <img src={} alt=""/> */}
+                        <img src={img1} alt="Círculo de quatro pessoas"/>
                         <figcaption>
-
+                        Oferece livros a quem não tem acesso, ajudando a reduzir a exclusão social.
                         </figcaption>
                     </figure>
                     <figure>
-                        {/* <img src={} alt=""/> */}
+                        <img src={img2} alt="Uma pessoa lendo"/>
                         <figcaption>
-
+                        Estimula o hábito da leitura e o aprendizado contínuo.
                         </figcaption>
                     </figure>
                     <figure>
-                        {/* <img src={} alt=""/> */}
+                        <img src={img3} alt="Pessoas com pensamento coletivo"/>
                         <figcaption>
-
+                        Fornece conhecimento e inspiração, permitindo que indivíduos transformem suas vidas.
                         </figcaption>
                     </figure>
                     <figure>
-                        {/* <img src={} alt=""/> */}
+                        <img src={img4} alt="Uma balança antiga"/>
                         <figcaption>
-
+                        Garante que todos, independentemente de sua condição, tenham oportunidades de aprendizado.
                         </figcaption>
                     </figure>
                 </div>
