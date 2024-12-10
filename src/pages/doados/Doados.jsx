0a08adbc57ba3dbc33903a7d121bd4260dd4614c
Pollus Emanuel Zhudarak <emanuel@zdk.studio>
2024-12-10 17:01:56 -0300

    Estilizado a página Doados

--- src/pages/doados/Doados.jsx
+++ src/pages/doados/Doados.jsx
@@ -1,18 +1,19 @@
 import s from '../doados/doados.module.scss'
+import livroProtagonista from '../../assets/livroProtagonista.jpg'
 
 export default function Doados () {
     return (
         <section className={s.doados}>
             <h2>Livros Doados</h2>
             <div>
-                <section>
-                    <img src="" alt="livro - O Protagonista"/>
+                <article>
+                    <img src={livroProtagonista} alt="livro - Protagonista"/>
                     <div>
-                        <h3>O protagonista</h3>
+                        <h3>Protagonista</h3>
                         <p>Susanne Andrade</p>
                         <p>Ficção</p>
                     </div>
-                </section>
+                </article>
             </div>
         </section>
     )
